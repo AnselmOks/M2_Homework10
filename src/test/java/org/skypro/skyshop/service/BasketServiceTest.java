fa00442fbf9b3bc081a2f01c7e09be57f68fdf11
Andrey Moskvitin <anselm2007@mail.ru>
2026-03-18 13:41:59 +0300

    Module 2 homework 13

---
+++ src/test/java/org/skypro/skyshop/service/BasketServiceTest.java
@@ -0,0 +1,88 @@
+package org.skypro.skyshop.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.skypro.skyshop.exception.NoSuchProductException;
+import org.skypro.skyshop.model.basket.ProductBasket;
+import org.skypro.skyshop.model.product.Product;
+import org.skypro.skyshop.model.product.SimpleProduct;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BasketServiceTest {
+
+    @Mock
+    StorageService storageService;
+
+    @Mock
+    ProductBasket productBasket;
+
+    @InjectMocks
+    BasketService basketService;
+
+    private Map<UUID, Integer> getTestBasket(UUID testId, Integer col) {
+        Map<UUID, Integer> testBasket = new HashMap<>();
+        testBasket.put(testId, col);
+        return testBasket;
+    }
+
+    @DisplayName("Добавляется несуществующий товар в корзину")
+    @Test
+    public void whenAddNotExistProductToBasket() {
+        UUID testId = UUID.randomUUID();
+        when(storageService.getProductById(testId)).thenReturn(Optional.empty());
+        Exception exception = Assertions.assertThrows(NoSuchProductException.class,
+                () -> basketService.addProduct(testId));
+        Assertions.assertEquals("org.skypro.skyshop.exception.NoSuchProductException", exception.getClass().getName());
+        verify(productBasket, Mockito.never()).addProduct(testId);
+    }
+
+    @DisplayName("В корзину добавляется существующий товар")
+    @Test
+    public void whenAddExistProductToBasket() {
+        UUID testId = UUID.randomUUID();
+        Product testProduct = new SimpleProduct("Тестовый продукт", 100, testId);
+        when(storageService.getProductById(testId)).thenReturn(Optional.ofNullable(testProduct));
+        basketService.addProduct(testId);
+        verify(productBasket, Mockito.times(1)).addProduct(testId);
+    }
+
+    @DisplayName("Возвращается пустая корзина, если productBasket пуста")
+    @Test
+    public void returnEmptyUserBasketIfProductBasketIsEmpty() {
+        when(productBasket.getAllProducts()).thenReturn(new HashMap<>());
+        Assertions.assertTrue(basketService.getUserBasket().getBasketItems().isEmpty() &&
+                basketService.getUserBasket().getTotal() == 0);
+    }
+
+    @DisplayName("Возвращается не пустая корзина, если есть товары в productBasket")
+    @Test
+    public void returnNonEmptyUserBasketIfProductBasketIsNotEmpty() {
+        UUID testId = UUID.randomUUID();
+        Integer testCol = 2;
+        Map<UUID, Integer> testProductBasket = getTestBasket(testId, testCol);
+        when(productBasket.getAllProducts()).thenReturn(testProductBasket);
+
+        Product testProduct = new SimpleProduct("Тестовый продукт", 100, testId);
+        when(storageService.getProductById(testId)).thenReturn(Optional.ofNullable(testProduct));
+
+        basketService.addProduct(testId);
+
+        UserBasket userBasket = basketService.getUserBasket();
+        Assertions.assertTrue(!userBasket.getBasketItems().isEmpty() &&
+                userBasket.getTotal() == testProduct.getPrice() * testCol);
+    }
+
+}
---
+++ src/test/java/org/skypro/skyshop/service/SearchServiceTest.java
@@ -0,0 +1,68 @@
+package org.skypro.skyshop.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.skypro.skyshop.model.product.DiscountedProduct;
+import org.skypro.skyshop.model.product.FixPriceProduct;
+import org.skypro.skyshop.model.product.SimpleProduct;
+import org.skypro.skyshop.model.search.SearchResult;
+import org.skypro.skyshop.model.search.Searchable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class SearchServiceTest {
+
+    @Mock
+    StorageService storageService;
+
+    @InjectMocks
+    SearchService searchService;
+
+    private List<Searchable> getTestData() {
+        List<Searchable> testData = new ArrayList<>();
+        testData.add(new SimpleProduct("Помидор", 300, UUID.randomUUID()));
+        testData.add(new FixPriceProduct("Груша", UUID.randomUUID()));
+        testData.add(new FixPriceProduct("Мандарин", UUID.randomUUID()));
+        testData.add(new DiscountedProduct("Лук", 100, 20, UUID.randomUUID()));
+        testData.add(new DiscountedProduct("Арбуз", 450, 10, UUID.randomUUID()));
+        testData.add(new SimpleProduct("Морковь", 300, UUID.randomUUID()));
+        return testData;
+    }
+
+    @DisplayName("Хранилище пустое")
+    @Test
+    public void whenStorageIsEmpty() {
+        List<SearchResult> results = searchService.search("Помидор");
+        assertEquals(results, Collections.emptyList());
+    }
+
+    @DisplayName("Хранилище не пустое, но товар не найден")
+    @Test
+    public void whenStorageIsNotEmptyButNotFound() {
+        List<Searchable> testData = getTestData();
+        when(storageService.getAllData()).thenReturn(testData);
+        List<SearchResult> results = searchService.search("Нет такой строки");
+        assertEquals(results, Collections.emptyList());
+    }
+
+    @DisplayName("Хранилище не пустое, и товар найден")
+    @Test
+    public void whenStorageIsNotEmptyAndFound() {
+        List<Searchable> testData = getTestData();
+        when(storageService.getAllData()).thenReturn(testData);
+        List<SearchResult> results = searchService.search("Помидор");
+        assertEquals(results.size(), 1);
+    }
+
+}
